1d488dbc6c4a8627dc1ab218b459f38c5988ab49
csvitor-dev <csvitor.dev@gmail.com>
2024-10-20 10:45:38 -0300

    feat: adding and updating exception messages with 'resource files'

--- src/BackEnd/RecipesApp.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/BackEnd/RecipesApp.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using RecipesApp.Communication.Requests;
+using RecipesApp.Exception.Resources;
 
 namespace RecipesApp.Application.UseCases.User.Register;
 
@@ -7,12 +8,17 @@ internal class RegisterUserValidator : AbstractValidator<RegisterUserRequestJSON
 {
     internal RegisterUserValidator()
     {
-        RuleFor((u) => u.Name).NotNull().NotEmpty().WithMessage("The Name field is required");
+        RuleFor((u) => u.Name).NotNull().NotEmpty()
+            .WithMessage(ResourcesAccessor.NAME_REQUIRED);
 
-        RuleFor((u) => u.Email).NotNull().NotEmpty().WithMessage("The Email field is required");
-        RuleFor((u) => u.Email).EmailAddress().WithMessage("Email isn't valid");
+        RuleFor((u) => u.Email).NotNull().NotEmpty()
+            .WithMessage(ResourcesAccessor.EMAIL_REQUIRED);
+        RuleFor((u) => u.Email).EmailAddress()
+            .WithMessage(ResourcesAccessor.EMAIL_INVALID);
 
-        RuleFor(u => u.Password).NotNull().NotEmpty().WithMessage("The Password field is required");
-        RuleFor((u) => u.Password.Length).GreaterThanOrEqualTo(6).WithMessage("Password must be at least 6 characters along");
+        RuleFor(u => u.Password).NotNull().NotEmpty()
+            .WithMessage(ResourcesAccessor.PASSWORD_REQUIRED);
+        RuleFor((u) => u.Password.Length).GreaterThanOrEqualTo(6)
+            .WithMessage(ResourcesAccessor.PASSWORD_LENGTH);
     }
 }
---
+++ src/Shared/RecipesApp.Exception/Resources/ResourcesAccessor.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipesApp.Exception.Resources;
+
+public static class ResourcesAccessor
+{
+    public static string EMAIL_INVALID { get => ExceptionMessagesResource.EMAIL_INVALID; }
+    public static string EMAIL_REQUIRED { get => ExceptionMessagesResource.EMAIL_REQUIRED; }
+    public static string NAME_REQUIRED { get => ExceptionMessagesResource.NAME_REQUIRED; }
+    public static string PASSWORD_LENGTH { get => ExceptionMessagesResource.PASSWORD_LENGTH; }
+    public static string PASSWORD_REQUIRED { get => ExceptionMessagesResource.PASSWORD_REQUIRED; }
+}
