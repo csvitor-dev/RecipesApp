0b70da6d283a278a4615fdd7f84333914632c461
csvitor-dev <csvitor.dev@gmail.com>
2024-10-20 09:55:56 -0300

    feat(API): adding endpoint to register an user

---
+++ src/BackEnd/RecipesApp.API/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipesApp.Communication.Requests;
+using RecipesApp.Communication.Responses;
+
+namespace RecipesApp.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(RegisterUserResponseJSON), StatusCodes.Status201Created)]
+    public IActionResult Register(RegisterUserRequestJSON request)
+    {
+        RegisterUserResponseJSON response = new(request.Name);
+        
+        return Created(string.Empty, response);
+    }
+}
---
+++ src/Shared/RecipesApp.Communication/Requests/RegisterUserRequestJSON.cs
@@ -0,0 +1,3 @@
+﻿namespace RecipesApp.Communication.Requests;
+
+public record RegisterUserRequestJSON(string Name, string Email, string Password);
