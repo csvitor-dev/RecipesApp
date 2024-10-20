9fe7d34c1ebf9c7f62ab0818f0266461389684a7
csvitor-dev <csvitor.dev@gmail.com>
2024-10-20 18:08:37 -0300

    feat(data): adding db context class and segregated repos by entity

---
+++ src/BackEnd/RecipesApp.Infra/Data/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipesApp.Domain.Entities;
+
+namespace RecipesApp.Infra.Data.Repositories;
+public class UserRepository(RecipesAppContext context)
+{
+    private readonly RecipesAppContext _context = context;
+
+    public async Task AddUserAsync(User user)
+        =>  await _context.Users.AddAsync(user);
+
+    public async Task<bool> ExistsActiveUserWithEmail(string email)
+    {
+        var result = _context.Users
+            .AnyAsync((u) => u.Email.Equals(email) && u.Active);
+
+        return await result;
+    }
+}
