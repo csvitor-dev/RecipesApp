138a5d44089c5dda8e6b763feb3bc4cf96aa3e32
csvitor-dev <csvitor.dev@gmail.com>
2024-11-08 17:22:59 -0300

    chore: applying DI for 'FluentMigrator' dependencies in '.Infra'

--- src/BackEnd/RecipesApp.Infra/Extensions/InfrastructureDIExtension.cs
+++ src/BackEnd/RecipesApp.Infra/Extensions/InfrastructureDIExtension.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using FluentMigrator.Runner;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RecipesApp.Domain.Repositories;
@@ -14,6 +16,7 @@ public static class InfrastructureDIExtension
     {
         AddDbContext(self,
             configuration.ConnectionString());
+        AddFluentMigrator(self, configuration.ConnectionString());
         AddRepositories(self);
     }
 
@@ -32,5 +35,16 @@ public static class InfrastructureDIExtension
         services.AddScoped<IUserReadOnlyRepository, UserRepository>();
         services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
         services.AddScoped<IUnitOfWork, PersistenceUnitOfWork>();
+    }
+
+    private static void AddFluentMigrator(IServiceCollection services, string? connection)
+    {
+        services.AddFluentMigratorCore().ConfigureRunner(options =>
+        {
+            options.AddSqlServer()
+                .WithGlobalConnectionString(connection ??
+                                            throw new InvalidOperationException("Connection string is missing"))
+                .ScanIn(Assembly.Load("RecipesApp.Infra")).For.All();
+        });
     }
 }
