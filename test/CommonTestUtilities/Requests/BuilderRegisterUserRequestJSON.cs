70295e1896ba0ad205eb28eaa38233aff51d8306
csvitor-dev <csvitor.dev@gmail.com>
2024-11-17 12:52:58 -0300

    feat: implementing the resource to the test case, test passed

--- test/CommonTestUtilities/Requests/BuilderRegisterUserRequestJSON.cs
+++ test/CommonTestUtilities/Requests/BuilderRegisterUserRequestJSON.cs
@@ -3,13 +3,25 @@ using RecipesApp.Communication.Requests;
 
 namespace CommonTestUtilities.Requests;
 
-public class BuilderRegisterUserRequestJSON
+public static class BuilderRegisterUserRequestJSON
 {
-    public static RegisterUserRequestJSON Build()
+    public static RegisterUserRequestJSON CreateMock()
+        => new Faker<RegisterUserRequestJSON>()
+            .CustomInstantiator(f =>
+            {
+                var name = f.Person.FirstName;
+
+                return new RegisterUserRequestJSON(
+                    name,
+                    f.Internet.Email(name),
+                    f.Internet.Password()
+                );
+            }).Generate();
+
+    public static RegisterUserRequestJSON CreateMockWithOutName()
         => new Faker<RegisterUserRequestJSON>()
             .CustomInstantiator(f => new RegisterUserRequestJSON(
-                f.Person.FirstName,
-                f.Internet.Email(f.Person.FirstName),
-                f.Internet.Password()
+                Email: f.Internet.Email(),
+                Password: f.Internet.Password()
             )).Generate();
 }
