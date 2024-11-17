1f1b379a5ac1d29193e6cde78818ec419f2ab236
csvitor-dev <csvitor.dev@gmail.com>
2024-11-17 12:52:24 -0300

    test: creating test case for 'RegisterUserValidator' on failure without name, test failed

--- test/Validators.Test/User/Register/RegisterUserValidatorTest.cs
+++ test/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -1,18 +1,35 @@
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 using RecipesApp.Application.UseCases.User.Register;
+using RecipesApp.Exception.Resources;
 
 namespace Validators.Test.User.Register;
 
-public class RegisterUserValidatorTest
+public class RegisterUserValidatorTest : IDisposable
 {
+    private readonly RegisterUserValidator _validator = new();
+
+    public void Dispose()
+        => GC.SuppressFinalize(this);
+    
     [Fact]
     public void Test_OnSuccess()
     {
-        RegisterUserValidator v = new();
-        var request = BuilderRegisterUserRequestJSON.Build();
-        var result = v.Validate(request);
+        var request = BuilderRegisterUserRequestJSON.CreateMock();
+        var result = _validator.Validate(request);
 
         result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Test_OnFailureWithEmptyName()
+    {
+        var request = BuilderRegisterUserRequestJSON.CreateMockWithOutName();
+        
+        var result = _validator.Validate(request!);
+        
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle()
+            .And.Contain(e => e.ErrorMessage.Equals(ResourcesAccessor.NAME_REQUIRED));
     }
 }
