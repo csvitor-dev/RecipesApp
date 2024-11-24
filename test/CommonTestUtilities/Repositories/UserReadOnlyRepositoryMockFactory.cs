5751bee06beef5d2829ccc0fa7881ea96a23d46e
csvitor-dev <csvitor.dev@gmail.com>
2024-11-24 18:13:47 -0300

    feat(UserReadOnlyRepositoryMockFactory): overrides the default return of the repository method

--- test/CommonTestUtilities/Repositories/UserReadOnlyRepositoryMockFactory.cs
+++ test/CommonTestUtilities/Repositories/UserReadOnlyRepositoryMockFactory.cs
@@ -6,7 +6,12 @@ namespace CommonTestUtilities.Repositories;
 public class UserReadOnlyRepositoryMockFactory
 {
     private readonly Mock<IUserReadOnlyRepository> _repository = new();
-    
+
+    public void ExistActiveUserWithEmail(string email)
+        => _repository.Setup(repo
+            => repo.ExistsActiveUserWithEmailAsync(email)
+        ).ReturnsAsync(true);
+
     public IUserReadOnlyRepository CreateMock()
         => _repository.Object;
 }
