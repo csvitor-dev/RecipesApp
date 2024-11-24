23af7662e7625bdf73486e84b7d17344017e41c8
csvitor-dev <csvitor.dev@gmail.com>
2024-11-24 18:15:56 -0300

    test(RegisterUserUC): create a test case when the use case fails, the test passes

--- test/UseCases.Test/User/Register/RegisterUserUCTest.cs
+++ test/UseCases.Test/User/Register/RegisterUserUCTest.cs
@@ -3,22 +3,27 @@ using CommonTestUtilities.Requests;
 using CommonTestUtilities.Services;
 using FluentAssertions;
 using RecipesApp.Application.UseCases.User.Register;
+using RecipesApp.Exception.Project;
+using RecipesApp.Exception.Resources;
 
 namespace UseCases.Test.User.Register;
 
 public class RegisterUserUCTest
 {
-    private RegisterUserUC CreateUseCase()
+    private RegisterUserUC CreateUseCase(string? email = null)
     {
-        var r = new UserReadOnlyRepositoryMockFactory().CreateMock();
+        var factory = new UserReadOnlyRepositoryMockFactory();
+
+        if (string.IsNullOrEmpty(email) is false)
+            factory.ExistActiveUserWithEmail(email);
         var w = UserWriteOnlyRepositoryMockFactory.CreateMock();
         var uw = UnitOfWorkMockFactory.CreateMock();
         var map = MapperMockFactory.CreateMock();
         var pw = EncryptMockFactory.CreateMock();
-        
-        return new RegisterUserUC(r, w, uw, map, pw);
+
+        return new RegisterUserUC(factory.CreateMock(), w, uw, map, pw);
     }
-    
+
     [Fact]
     public async Task Test_OnSuccess()
     {
@@ -29,5 +34,18 @@ public class RegisterUserUCTest
 
         result.Should().NotBeNull();
         result.Name.Should().Be(request.Name);
+    }
+
+    [Fact]
+    public async Task Test_OnFailure_WithEmailAlreadyRegistered()
+    {
+        var request = RegisterUserRequestJSONMockFactory.CreateMock();
+        var uc = CreateUseCase(request.Email);
+
+        Func<Task> act = async () => await uc.Execute(request);
+
+        (await act.Should().ThrowAsync<ErrorOnValidationException>())
+            .Where(e => e.ErrorMessages.Count == 1
+                        && e.ErrorMessages.Contains(ResourcesAccessor.EMAIL_ALREADY_REGISTERED));
     }
 }
